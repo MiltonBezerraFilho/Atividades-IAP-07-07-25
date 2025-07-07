824ee361dc8c7109ff142ded3673f8ce3030247c
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-07 11:11:12 -0300

    Add files via upload

---
+++ calculadora_de_peso_ideal.py
@@ -0,0 +1,4 @@
+h = float(input("Insira a altura em metro usando o ponto como indicador de casa decimal: "))
+hh = (72.7 * h) - 58
+hm = (62.1 * h) -44.7
+print ("O peso ideal masculino é:", hh, "e o peso ideal feminino é", hm)
