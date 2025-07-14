2de9871bb7cf0932daac51d52c9ec4c0ee7c6bbf
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-14 11:39:46 -0300

    Update calculadora_de_salario.py

--- calculadora_de_salario.py
+++ calculadora_de_salario.py
@@ -5,4 +5,4 @@ imposto_de_renda = salario_bruto * 0.11
 inss = salario_bruto *  0.08
 sindicato = salario_bruto * 0.05
 salario_liquido = salario_bruto - imposto_de_renda - inss - sindicato
-print ("Salário Bruto: R$", salario_bruto, "\nIR(11%): R$", imposto_de_renda, "\nINSS(8%): R$", inss, "\nSindicato(5%): R$", sindicato, "\nLíquido: R$", salario_liquido)
+print (f'Salário Bruto: R${salario_bruto} \nIR(11%): R${imposto_de_renda} \nINSS(8%): R${inss} \nSindicato(5%): R${sindicato} \nLíquido: R${salario_liquido}')
